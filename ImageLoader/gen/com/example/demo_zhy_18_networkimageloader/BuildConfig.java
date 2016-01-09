3e6a850a5f98dafb7702a898f5cb2e795e7e3ba0
tytredhat <769146186@qq.com>
2016-01-09 10:56:28 +0800

    1

--- ImageLoader/gen/com/example/demo_zhy_18_networkimageloader/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.demo_zhy_18_networkimageloader;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
